ac57a02d8cbaebf72d709d86ab239784e5fc7c04
kendalwon <kendalwon@gmail.com>
2020-07-09 20:34:13 -0400

    Updates

---
+++ precache-manifest.95f80fcfd618dd1ca3281dd9d776708c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3604fd65dca68bde177e6c5945cd0a32",
+    "url": "/local-events/index.html"
+  },
+  {
+    "revision": "500c9df3cdd126189cb9",
+    "url": "/local-events/static/css/main.1cd65019.chunk.css"
+  },
+  {
+    "revision": "dab752c29cf9f16157e8",
+    "url": "/local-events/static/js/2.b7061b69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/local-events/static/js/2.b7061b69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "500c9df3cdd126189cb9",
+    "url": "/local-events/static/js/main.632379b0.chunk.js"
+  },
+  {
+    "revision": "a164783cb567aca8dfb6",
+    "url": "/local-events/static/js/runtime-main.a278337b.js"
+  }
+]);
